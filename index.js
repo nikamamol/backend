61be6730bab2f9bf3fbfdb66fff9c06569adf6cf
unknown <amolspatil018@gmail.com>
2023-10-28 15:56:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+require("dotenv").config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/twiter', (req, res) => {
+    res.send('Twiter Amol')
+})
+app.get('/login', (req, res) => {
+    res.send("<h1>Please llogin at chai or code</h1>")
+})
+app.get("/youtube", (req, res) => {
+    res.send('<h2>AMol youtube</h2>')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${port}`)
+})
